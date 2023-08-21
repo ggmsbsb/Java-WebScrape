c9546611df2b941ec647e1bec5d9e05f74515bc2
ggmsbsb <guilhermem20g@gmail.com>
2023-08-20 21:15:15 -0300

    Style change

--- src/main/java/com/example/webscraperjavafx/scrapeApp.java
+++ src/main/java/com/example/webscraperjavafx/scrapeApp.java
@@ -4,7 +4,9 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class scrapeApp extends Application {
 
@@ -12,26 +14,39 @@ public class scrapeApp extends Application {
     public void start(Stage primaryStage) {
         try {
             System.out.println("Loading FXML...");
-            //Create and intialize FXML
+            // Create and initialize FXML
             FXMLLoader loader = new FXMLLoader(getClass().getResource("hello-view.fxml"));
             Parent root = loader.load();
             System.out.println("FXML loaded successfully.");
 
-            //App style
-            primaryStage.setTitle("Web Scraper JavaFX Application");
-            primaryStage.setScene(new Scene(root, 800, 600));
+            // Access the root element of the loaded FXML
+            AnchorPane anchorPane = (AnchorPane) root;
 
-            //Main window
+            // Create a stage with no decorations (borderless)
+            primaryStage.initStyle(StageStyle.UNDECORATED);
+
+            // Get the preferred width and height from the root element
+            double preferredWidth = anchorPane.getPrefWidth();
+            double preferredHeight = anchorPane.getPrefHeight();
+
+            // Set the scene with the preferred width and height
+            Scene scene = new Scene(root, preferredWidth, preferredHeight);
+            primaryStage.setScene(scene);
+
+            // Title is not displayed in a borderless window, but you can set it as you wish
+            primaryStage.setTitle("Web Scraper JavaFX Application");
+
+            // Main window
             primaryStage.show();
 
         } catch (Exception e) {
-            //Exception handling
+            // Exception handling
             System.err.println("Error loading FXML: " + e.getMessage());
             e.printStackTrace();
         }
     }
 
-    //Inicia o App
+    // Inicia o App
     public static void main(String[] args) {
         launch(args);
     }
